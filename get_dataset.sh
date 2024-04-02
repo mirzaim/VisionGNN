1ab4c072b6ab976cd662f5d983bc62bfdfd2c142
Morteza Mirzaei <mortezamirzai120@gmail.com>
2024-04-02 11:23:59 -0400

    Add some documents to get_dataset.sh file.

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# For more information, visit: https://github.com/fastai/imagenette
+
 dataset_name="imagenette2-320"
 dataset_url="https://s3.amazonaws.com/fast-ai-imageclas/${dataset_name}.tgz"
 
