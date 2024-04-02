5991f58a6c20a972cca5c8e4b8dd767988181870
Morteza Mirzaei <mortezamirzai120@gmail.com>
2024-04-02 11:24:22 -0400

    Update Model.

--- model.py
+++ model.py
@@ -5,20 +5,23 @@ from utility import SimplePatchifier, TwoLayerNN
 
 
 class ViGBlock(nn.Module):
-    def __init__(self, in_features, num_edges=8):
+    def __init__(self, in_features, num_edges=9):
         super().__init__()
+        self.k = num_edges
         self.num_edges = num_edges
         self.in_layer1 = TwoLayerNN(in_features)
         self.out_layer1 = TwoLayerNN(in_features)
-        self.in_layer2 = TwoLayerNN(in_features)
-        self.out_layer2 = TwoLayerNN(in_features)
+        self.droppath1 = nn.Identity() # DropPath(0)
+        self.in_layer2 = TwoLayerNN(in_features, in_features*4)
+        self.out_layer2 = TwoLayerNN(in_features, in_features*4)
+        self.droppath2 = nn.Identity() # DropPath(0)
         self.fc = nn.Linear(in_features*2, in_features)
 
     def forward(self, x):
         B, N, C = x.shape
 
         sim = x @ x.transpose(-1, -2)
-        graph = sim.topk(5, dim=-1).indices
+        graph = sim.topk(self.k, dim=-1).indices
 
         shortcut = x
         x = self.in_layer1(x.view(B * N, -1)).view(B, N, -1)
@@ -34,26 +37,45 @@ class ViGBlock(nn.Module):
         # PyTorch has nn.MultiheadAttention, maybe useful.
         x = self.fc(x.view(B * N, -1)).view(B, N, -1)
 
-        x = self.out_layer1(F.relu(x).view(B * N, -1)).view(B, N, -1)
+        x = self.droppath1(self.out_layer1(F.gelu(x).view(B * N, -1)).view(B, N, -1))
         x = x + shortcut
 
-        x = self.out_layer2(F.relu(self.in_layer2(
-            x.view(B * N, -1)))).view(B, N, -1) + x
+        x = self.droppath2(self.out_layer2(F.gelu(self.in_layer2(
+            x.view(B * N, -1)))).view(B, N, -1)) + x
 
         return x
 
 
 class VGNN(nn.Module):
-    def __init__(self, in_features=256*3, num_patches=196, num_ViGBlocks=16):
+    def __init__(self, in_features=3*16*16, out_feature=320, num_patches=196, num_ViGBlocks=16):
         super().__init__()
 
         self.patchifier = SimplePatchifier()
-        self.patch_embedding = TwoLayerNN(in_features)
+        # self.patch_embedding = TwoLayerNN(in_features)
+        self.patch_embedding = nn.Sequential(
+            nn.Linear(in_features, out_feature//2),
+            nn.BatchNorm1d(out_feature//2),
+            nn.GELU(),
+            nn.Linear(out_feature//2, out_feature//4),
+            nn.BatchNorm1d(out_feature//4),
+            nn.GELU(),
+            nn.Linear(out_feature//4, out_feature//8),
+            nn.BatchNorm1d(out_feature//8),
+            nn.GELU(),
+            nn.Linear(out_feature//8, out_feature//4),
+            nn.BatchNorm1d(out_feature//4),
+            nn.GELU(),
+            nn.Linear(out_feature//4, out_feature//2),
+            nn.BatchNorm1d(out_feature//2),
+            nn.GELU(),
+            nn.Linear(out_feature//2, out_feature),
+            nn.BatchNorm1d(out_feature)
+        )
         self.pose_embedding = nn.Parameter(
-            torch.rand(num_patches, in_features))
+            torch.rand(num_patches, out_feature))
 
         self.blocks = nn.Sequential(
-            *[ViGBlock(in_features) for _ in range(num_ViGBlocks)])
+            *[ViGBlock(out_feature) for _ in range(num_ViGBlocks)])
 
     def forward(self, x):
         x = self.patchifier(x)
